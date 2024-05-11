8dfd2dbd78555cd4db63aa0dbe72dc9b0b1eab4f
fusagi <ruth.jam23@gmail.com>
2024-05-11 20:12:59 +0200

    Add outro animations

--- scenes/LoseScreen/LoseScreen.gd
+++ scenes/LoseScreen/LoseScreen.gd
@@ -1,1 +1,7 @@
 extends "res://addons/maaacks_game_template/extras/scenes/LoseScreen/LoseScreen.gd"
+
+func _ready():
+	$Outro/AnimationPlayer.play("outro_lose_animation")
+
+func _on_outro_animation_finished():
+	$Outro.queue_free()
