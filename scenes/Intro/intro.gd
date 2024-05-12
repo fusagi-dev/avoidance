0cb97655db7f0841f3e09ea658195db6f26d52c1
fusagi <ruth.jam23@gmail.com>
2024-05-12 19:40:25 +0200

    Make animations skippable

--- scenes/Intro/intro.gd
+++ scenes/Intro/intro.gd
@@ -5,5 +5,9 @@ extends Node
 signal animation_finished
 
 func _on_animation_player_animation_finished(anim_name):
+	SceneLoader.load_scene(game_scene_path)
+
+
+func _on_skip_button_pressed():
 	emit_signal("animation_finished")
-	#SceneLoader.load_scene(game_scene_path)
+	$AnimationPlayer.queue_free()
